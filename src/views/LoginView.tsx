b2b27f3024200b01144aa0ee7cd1ffa7c6e0f434
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-02-20 17:00:19 -0300

    IMAGEN y ESTILOS v1. Se agregan estilos iniciales y la imagen lateral

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,36 +1,60 @@
 "use client";
 import { validateLoginForm } from '@/helpers/validate';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import Image from 'next/image';
 
 const LoginView = () => {
     return (
+      <div className="flex flex-row ">
         <div>
-        <h1>Iniciar Sesión</h1>
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          validate={validateLoginForm}
-          onSubmit={(values, { setSubmitting }) => {
-            console.log("Submit exitoso")
-            // setTimeout(() => {
-            //   alert(JSON.stringify(values, null, 2));
-            //   setSubmitting(false);
-            // }, 400);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-                <label>Email: </label>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" component="div" />
-                <label>Contraseña: </label>
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" component="div" />
-                <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
+          <h1>Iniciar Sesión</h1>
+          <p>Ingrese sus datos para acceder</p>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validate={validateLoginForm}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log("Submit exitoso");
+              // setTimeout(() => {
+              //   alert(JSON.stringify(values, null, 2));
+              //   setSubmitting(false);
+              // }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <div>
+
+                  <div>
+                    <div>Email: </div>
+                    <Field type="email" name="email" />
+                    <ErrorMessage name="email" component="div" />
+                  </div>
+
+                  <div>
+                    <div>Contraseña: </div>
+                    <Field type="password" name="password" />
+                    <ErrorMessage name="password" component="div" />
+                  </div>
+
+                  <button type="submit" disabled={isSubmitting}>
+                    Submit
+                  </button>
+                  
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+
+        <div>
+          <Image
+          src='/images/Login.png'
+          alt=''
+          width={300}
+          height={300}
+          className="box-border"
+          />
+        </div>
       </div>
     );
 }
