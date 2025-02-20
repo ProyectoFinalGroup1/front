56d6b2a80135350a957b57b861e2779ab26c00c1
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-02-20 13:58:57 -0300

    USE CLIENT y FORMIK. Se declara el uso de cliente y se importan las funciones iniciales de formik segun docu

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,8 +1,34 @@
+"use client";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 const LoginView = () => {
     return (
         <div>
-            <h1>Este es el LoginView</h1>
-        </div>
+        <h1>Iniciar Sesión</h1>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log("Submit exitoso")
+            // setTimeout(() => {
+            //   alert(JSON.stringify(values, null, 2));
+            //   setSubmitting(false);
+            // }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     );
 }
 
