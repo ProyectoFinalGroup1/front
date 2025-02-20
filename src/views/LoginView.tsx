1f0a2bbdd0cbc2caaf5a0a5c8c2c3fb5071eeaa6
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-02-20 18:46:14 -0300

    ESTILOS v2. Se centra el contenido. Hay un problema con aplicar PADDING y MARGIN

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -5,55 +5,56 @@ import Image from 'next/image';
 
 const LoginView = () => {
     return (
-      <div className="flex flex-row ">
-        <div>
-          <h1>Iniciar Sesión</h1>
-          <p>Ingrese sus datos para acceder</p>
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validate={validateLoginForm}
-            onSubmit={(values, { setSubmitting }) => {
-              console.log("Submit exitoso");
-              // setTimeout(() => {
-              //   alert(JSON.stringify(values, null, 2));
-              //   setSubmitting(false);
-              // }, 400);
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <div>
-
+      <div className=''>
+        <div className="flex flex-row justify-center items-center
+                        mt-20">
+          <div>
+            <h1>Iniciar Sesión</h1>
+            <p>Ingrese sus datos para acceder</p>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validate={validateLoginForm}
+              onSubmit={(values, { setSubmitting }) => {
+                console.log("Submit exitoso");
+                // setTimeout(() => {
+                //   alert(JSON.stringify(values, null, 2));
+                //   setSubmitting(false);
+                // }, 400);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
                   <div>
-                    <div>Email: </div>
-                    <Field type="email" name="email" />
-                    <ErrorMessage name="email" component="div" />
+                    <div>
+                      <div>Email: </div>
+                      <Field type="email" name="email" />
+                      <ErrorMessage name="email" component="div" />
+                    </div>
+
+                    <div>
+                      <div>Contraseña: </div>
+                      <Field type="password" name="password" />
+                      <ErrorMessage name="password" component="div" />
+                    </div>
+
+                    <button type="submit" disabled={isSubmitting}>
+                      Submit
+                    </button>
                   </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
 
-                  <div>
-                    <div>Contraseña: </div>
-                    <Field type="password" name="password" />
-                    <ErrorMessage name="password" component="div" />
-                  </div>
-
-                  <button type="submit" disabled={isSubmitting}>
-                    Submit
-                  </button>
-                  
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
-
-        <div>
-          <Image
-          src='/images/Login.png'
-          alt=''
-          width={300}
-          height={300}
-          className="box-border"
-          />
+          <div>
+            <Image
+              src="/images/Login.png"
+              alt=""
+              width={300}
+              height={300}
+              className="box-border"
+            />
+          </div>
         </div>
       </div>
     );
