e20757b6c65b3b20a5a335bd951e0dd28604c3ff
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-02-20 19:18:47 -0300

    ESTILOS. Se aplican estilos al footer en la misma linea de navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
     return (
-        <div>
-            <p>Esto es el Footer</p>
-        </div>
+      <footer className="bg-white bg-opacity-30 shadow-md
+                        w-full
+                        absolute inset-x-0 bottom-0
+                        py-4 text-center">
+        <div>Esto es el Footer</div>
+      </footer>
     );
 }
 
