7bc23bd2cba4acd9dea58d862252aab20cda7345
Victor <espinozagallegosvictor@gmail.com>
2025-02-25 14:30:56 -0300

    TIPADO 3. Se crea la interfaz para el PROVIDER y se aplica. Se tipa el contenido del provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,26 @@
 'use client'
+import { IUserSession } from "@/types";
 import { createContext, useContext, useState, useEffect } from "react"
 
-export const AuthContext = createContext({
+// Interface
+export interface AuthContextProps {
+    userData: IUserSession | null,
+    setUserData: (userData: IUserSession | null ) => void
+}
+
+export const AuthContext = createContext<AuthContextProps>({
     userData: null,
     setUserData: () => {}
 })
 
-export const AuthProvider = ({ children }) => {
+// Interface
+export interface AuthProviderProps {
+    children: React.ReactNode
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     // Estado con la info de usuario
-    const [userData, setUserData] = useState();
+    const [userData, setUserData] = useState<IUserSession | null>(null);
     
     // Hook para guardar en localStorage
     useEffect(() => {
