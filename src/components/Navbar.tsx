8ba1ce9a9d93d1eff8192281f72dbdc966c8f323
Victor <espinozagallegosvictor@gmail.com>
2025-02-27 18:19:05 -0300

    CERRAR SESION por PERFIL. Se cambia el renderizado condicional de CERRAR SESION por PERFIL

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,12 @@ const Navbar = () => {
           <div className="flex items-center">
             {
               userData?.token ? (
-                <LogOutButton/>
+                <Link
+                  className="inline-flex items-center justify-center rounded-xl bg-green-800 px-4 py-2 text-lg font-bold text-white transition-all duration-150 hover:bg-green-500 hover:text-gray-900 [text-shadow:2px_2px_4px_rgba(0,0,0,0.8)] [webkit-text-stroke:1px_black]"
+                  href="/dashboard/user"
+              >
+                Perfil
+              </Link>
               ) : (
                 <Link
                   className="inline-flex items-center justify-center rounded-xl bg-green-800 px-4 py-2 text-lg font-bold text-white transition-all duration-150 hover:bg-green-500 hover:text-gray-900 [text-shadow:2px_2px_4px_rgba(0,0,0,0.8)] [webkit-text-stroke:1px_black]"
