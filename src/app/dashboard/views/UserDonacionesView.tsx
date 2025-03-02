785bd17d40f613180b1a6aa9bc3b05a392349f59
Victor <espinozagallegosvictor@gmail.com>
2025-03-02 19:59:08 -0300

    Se implementa la función para generar los pagos con cambios pendientes en comentarios

--- src/app/dashboard/views/UserDonacionesView.tsx
+++ src/app/dashboard/views/UserDonacionesView.tsx
@@ -1,8 +1,40 @@
+'use client';
 import React from 'react';
 
 const UserDonacionesView = () => {
+
+  const handleDonation = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/mercadopago/donar', { //http://localhost:3000/mercadopago/donar
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: 'victor@mail.cl',  // traer desde token el mail de usuario logueado
+          monto: 1,  // Especificar en pantalla monto de la donación
+          nombreMostrar: 'Victor Espinoza', // traer desde el token username de usuario logueado
+          mensajeAgradecimiento: 'Gracias por tu generosidad',
+          mostrarEnMuro: true,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        // Redirigir al usuario al enlace de pago de Mercado Pago
+        window.location.href = data.pagoInfo.init_point;
+      } else {
+        alert('Hubo un problema al generar el enlace de pago');
+      }
+    } catch (error) {
+      console.error('Error al procesar la donación:', error);
+      alert('Hubo un error al procesar tu donación');
+    }
+  };
+  
   return (
-    <div className="min-h-screen bg-gray-100 flex items-end py-10 px-4 md:px-10">
+    <div className="min-h-screen bg-gray-100 flex items-center py-10 px-4 md:px-10">
       <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-2xl p-6 w-full">
         <h1 className="text-2xl md:text-3xl font-semibold text-gray-800 text-center mb-4">
           Contribuye a un legado de amor
@@ -15,7 +47,8 @@ const UserDonacionesView = () => {
         <div className="space-y-4">
           <div className="bg-gray-50 p-4 rounded-lg shadow flex justify-between items-center">
             <span className="text-gray-700">Donación única</span>
-            <button className="bg-green-600 text-white px-4 py-2 rounded-lg shadow hover:bg-green-700 transition">
+            <button className="bg-green-600 text-white px-4 py-2 rounded-lg shadow hover:bg-green-700 transition"
+                    onClick={handleDonation}>
               Donar
             </button>
           </div>
