eb0e60f3c99e4f3f1faa93cb2a8d3636b1fc38b0
Victor <espinozagallegosvictor@gmail.com>
2025-03-02 17:33:17 -0300

    inicio de modulo

---
+++ src/views/DonacionesView.tsx
@@ -0,0 +1,11 @@
+const DonacionesView = () => {
+    return (
+        <div>
+            <h1>Donación exitosa</h1>
+            <h1>Donación pendiente</h1>
+            <h1>Donación fallida</h1>
+        </div>
+    );
+}
+
+export default DonacionesView;
