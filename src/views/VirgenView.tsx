5e1fea7c007017314ee3b305b7c5b5db3597a302
Victor <espinozagallegosvictor@gmail.com>
2025-03-07 13:15:54 -0300

    RENDERIZDAO CONDICIONAL. Se oculatan los mensajes que aun no han sido aprobados

--- src/views/VirgenView.tsx
+++ src/views/VirgenView.tsx
@@ -170,19 +170,32 @@ const VirgenView = () => {
           <div className="w-full mt-4">
             {messages.map((msg) => (
               <div key={msg.id} className="p-3 rounded-lg shadow-md mb-2 text-center">
+                {msg.estado ? (
+                  <>
+                    <p className={`text-gray-800 font-bold text-lg ${msg.estado ? '' : 'opacity-50'}`}>
+                      {msg.texto}
+                    </p>
 
-                <p className={`text-gray-800 font-bold text-lg ${msg.estado ? '' : 'opacity-50'}`}>
-                  {msg.texto}
-                </p>
+                    {msg.imagenUrl && (
+                      <div className="flex justify-center">
+                        <img src={msg.imagenUrl} alt="Imagen del mensaje" className="mt-2 max-w-xs rounded-lg" />
+                      </div>
+                    )}
 
-                {msg.imagenUrl && (
-                  <div className="flex justify-center">
-                    <img src={msg.imagenUrl} alt="Imagen del mensaje" className="mt-2 max-w-xs rounded-lg" />
+                    <p className="text-xs text-gray-500">
+                    Mensaje de {userData?.user.nombre} {userData?.user.apellido}. {new Date(msg.fechaPublicacion).toLocaleString()}
+                    </p>
+                  </>
+                ) : (
+                  <div>
+                  <p className="text-gray-500 italic">
+                    Plegaria pendiente de aprobación.
+                  </p>
+                  <p className="text-xs text-gray-500 mt-2">
+                    Fecha de solicitud {new Date(msg.fechaPublicacion).toLocaleString()}
+                  </p>
                   </div>
                 )}
-                <p className="text-xs text-gray-500">
-                Mensaje de {userData?.user.nombre} {userData?.user.apellido}. {new Date(msg.fechaPublicacion).toLocaleString()}
-                </p>
               </div>
             ))}
           </div>
