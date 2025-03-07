02442a62d153c1a19bfc3e0bccddaa3c57b1f91d
Victor <espinozagallegosvictor@gmail.com>
2025-03-07 14:08:45 -0300

    ESTILOS A MENSAJESADMIN. Se elimina una ruta sobrante, mensajesVirgen, porque se manejan todos los mensajes desde una sola ruta. Se agrega la vista de AdminVirgenView. Y se da un estilo inicial para separar las secciones de mensajes

---
+++ src/app/dashboard/views/AdminVirgenView.tsx
@@ -0,0 +1,11 @@
+const AdminVirgenView = () => {
+    return (
+        <div>
+            <h1>
+                Aqui se ACEPTAN/RECHAZAN las PLEGARIAS
+            </h1>
+        </div>
+    );
+}
+
+export default AdminVirgenView;
