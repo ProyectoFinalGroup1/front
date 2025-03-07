824e77f8c8de823d818f9e64cfdfaa9e763c0476
UrenaAramis <u.aramis2@gmail.com>
2025-03-07 14:23:56 -0300

    Sidebar Admin

--- src/app/dashboard/admin/AdminDashboardLayout.tsx
+++ src/app/dashboard/admin/AdminDashboardLayout.tsx
@@ -1,9 +1,10 @@
-import Sidebar from "@/components/Sidebar";
+import AdminSidebar from "@/components/AdminSidebar";
+
 
 const AdminDashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
+      <AdminSidebar />
       <main className="flex-1 p-6">{children}</main>
     </div>
   );
