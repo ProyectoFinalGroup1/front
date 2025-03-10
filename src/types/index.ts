806e7175a9ff5979dbcc109f2915d7dee78e4cde
Victor <espinozagallegosvictor@gmail.com>
2025-03-10 18:42:41 -0300

    Se descomenta una interface que producia error

--- src/types/index.ts
+++ src/types/index.ts
@@ -59,6 +59,19 @@ export interface IUserSession {
         password: string;
         provider?: string;
     }
+}
+export interface IUserDetails {
+  idUser: string;
+  nombre: string;
+  apellido: string;
+  email: string;
+  isAdmin: boolean;
+  dni: string;
+  recibirRecordatoriosAniversarios: boolean;
+  phoneNumber: string;
+  fechaPago: Date;
+}
+
 // }
 // export interface IPost {
 //   id: string;             // ID único de la publicación
@@ -70,16 +83,3 @@ export interface IUserSession {
 //   fechaPublicacion: string; 
 //   aprobada: boolean;    // Fecha de la última actualización en formato ISO
 // }
-
-//   export interface IUserDetails {
-//   idUser: string;
-//   nombre: string;
-//   apellido: string;
-//   email: string;
-//   isAdmin: boolean;
-//   dni: string;
-//   recibirRecordatoriosAniversarios: boolean;
-//   phoneNumber: string;
-//   fechaPago: Date;
-// 
-}
