d613dd37d30748b41494160e2c80ea72d0e13725
Victor <espinozagallegosvictor@gmail.com>
2025-03-10 12:54:52 -0300

    GESTION DE MENSAJES ADMIN. Se separan los mensajes de obituario y plegarias para mantener el orden del menu ADMIN. Se crea la ruta de plegarias a la virgen segun MENU DE ADMIN

--- src/app/dashboard/admin/mensajesVirgen/page.tsx
+++ src/app/dashboard/admin/mensajesVirgen/page.tsx
@@ -14,16 +14,17 @@ const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100 py-20">
       <h1 className="text-2xl font-bold mb-6">Gestión de mensajes</h1>
-      {/* Sección Obituario */}
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-4">
+
+      {/* Sección Plegarias a la Virgen */}
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-4 mb-4">
         <h2
           className="text-xl font-semibold cursor-pointer flex justify-between items-center"
-          onClick={() => toggleSection('obituario')}
+          onClick={() => toggleSection('virgen')}
         >
-          Obituario
-          <span>{openSection === 'obituario' ? '▲' : '▼'}</span>
+          Plegarias a la Virgen
+          <span>{openSection === 'virgen' ? '▲' : '▼'}</span>
         </h2>
-        {openSection === 'obituario' && <p>Mensajes de obituario aquí...</p>}
+        {openSection === 'virgen' && <AdminVirgenView />}
       </div>
     </div>
   );
