930c1e64865ecb047f0b67190c90b3b91d41381e
Victor <espinozagallegosvictor@gmail.com>
2025-03-11 23:31:33 -0300

    Llamada para traer todas las plegarias

--- src/app/dashboard/views/UserPlegariasView.tsx
+++ src/app/dashboard/views/UserPlegariasView.tsx
@@ -1,11 +1,56 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useAuth } from '@/context/AuthContext';
+import toast from 'react-hot-toast';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const UserPlegariasView = () => {
-    return(
-        <div>
-            <h1>
-                Recuento de plegarias por usuario
-            </h1>
-        </div>
-    );
-}
+  const { userData } = useAuth();
+
+  const [allMessagges, setAllMessagges] = useState<{id: string;
+                                                    texto: string;
+                                                    imagenUrl?: string;
+                                                    fechaPublicacion: string;
+                                                    estado: boolean;
+                                                    idUser: string;
+                                                }[]>([]);
+
+  useEffect(() => {
+    const fetchAllMessagges = async () => {
+      try {
+        const response = await fetch(`${API_URL}/mensajesVirgen`, {
+            method: "GET"
+        });
+        if (!response.ok) {
+            const errorText = await response.text();    // Intenta leer el mensaje del backend
+            throw new Error(`Error al obtener los mensajes: ${errorText}`);
+        }
+
+        const data = await response.json();
+        setAllMessagges(data);
+        console.log(data);
+
+      } catch (error) {
+        console.error("Error en fetAllchMessages:", error);
+      }
+    };
+
+    if (userData?.user.idUser) {
+      fetchAllMessagges();
+    }
+  
+  }, []);  // [userData?.user.idUser] Se volverá a montar cuando se loguee otro idUser
+
+
+    //  const parsedTexto = JSON.parse(mensaje.texto);
+    //  const texto = parsedTexto.texto;
+    //  const usuarioId = parsedTexto.usuarioId;
+    //  const estado = mensaje.estado;
+
+  return (
+    <div></div>
+  );
+};
 
 export default UserPlegariasView;
